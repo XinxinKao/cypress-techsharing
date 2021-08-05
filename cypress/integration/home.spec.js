17a719cbdb9646a55261e2ce9aa99731bcfc52ff
xinyi.kao <xinyi.kao@titansoft.com.sg>
2021-08-05 17:22:41 +0800

    Add test for click link to other page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -13,4 +13,9 @@ describe("Home page", () => {
         const expectedText = "Visit cypress.io to read the documentation";
         cy.contains(expectedText);
     });
+
+    it("click cypress link and then redirect to cypress website", () => {
+        cy.findByRole("link", { name: /visit cypress\.io/i }).click();
+        cy.url().should("eq", "https://www.cypress.io/");
+    });
   });
